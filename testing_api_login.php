61eacc60d297e2922fdcc9a19aa142b163a37944
Elias Tabello <etabw3@gmail.com>
2014-02-19 18:01:32 -0500

    working version 1

---
+++ testing_api_login.php
@@ -0,0 +1,60 @@
+<?php 
+    require_once("FoursquareAPI.class.php");
+    require_once("../../../secret.php");
+    // Set your client key and secret
+    $client_key = CLIENT_ID;
+    $client_secret = CLIENT_SECRET;
+    // Load the Foursquare API library
+
+    $foursquare = new FoursquareAPI($client_key,$client_secret);
+    $location = "Toronto, ON";
+
+    $lat = 43.768020441;
+    $lng = -79.229047273;
+    
+    //prepare params
+    $params = array('ll' => "$lat, $lng") ;
+
+    // Perform a request to a public resource
+    $response = $foursquare->GetPublic("venues/search",$params);
+    $venues = json_decode($response);
+
+    $nested = array();
+
+    print "number of venues returned was: " . count($venues->response->venues) . "<br /><hr><br />";
+
+    foreach($venues->response->venues as $venue)
+    {
+        #print_r($venue);
+        
+        foreach($venue as $key => $value)
+        {
+            if (is_object($key) or is_object($value))
+            {
+                if (!in_array($nested, $key))
+                {
+                    array_push($nested, $key);
+                }
+
+                print $key . ": ";
+                
+                foreach($value as $nested_key => $nested_value)
+                {
+                    if (!is_object($nested_value))
+                    {
+                        print $nested_key . ": " . $nested_value . "<br />";
+                    }
+                }       
+            }
+            else {
+                print $key . ": " . $value . "<br />";
+            }
+        }
+        
+        print "<br /><br /><hr><br /><br />";
+   
+    }
+    
+?>
+
+
---
+++ wfs.php
@@ -0,0 +1,61 @@
+<?php
+    require_once("FoursquareAPI.class.php");
+    require_once('RestServer.php');
+
+    // http://wfs.openciti.ca?method=MethodName&param1=Param1Value&param2=Param2Value
+    class WarFareSquare
+    {
+        //example of a function
+        public function hello($name){return(array("response" => "Hello $name"));}
+
+        
+        #each endpoint has its own function
+        
+        //please md5 encode the password
+        public function register_user($username, $password, $first, $last, $lat=-1, $lng=-1) 
+        {
+            //must be repeated for each api endpoint function due to RestServer functionality
+            $mongo = new MongoClient();
+            $wfs = $mongo->selectDB('wfs');
+            $users = $wfs->selectCollection('users');
+            $users->ensureIndex(array("username" => 1), array("unique" => 1));
+            $foursquare = new FoursquareAPI($client_key,$client_secret);
+            //-------------------------------------------------------------------------------
+
+            $return_code = 'null';
+            $insert_array = null;
+
+            try{
+                $insert_array = array('username' => $username, 
+                                      'password' => $password, 
+                                      'first' => $first , 
+                                      'last' => $last, 
+                                      'lat' => $lat,
+                                      'lng' => $lng);
+
+                $users->insert($insert_array);
+                $return_code = 'ok';
+            }
+            catch(MongoCursorException $e) 
+            {
+                return array("response" => "duplicate user");
+            }
+            catch(MongoException $e)
+            {
+                return array("response" => "fail");
+            }
+
+
+
+            $insert_array["response"] = $return_code;            
+            return array($insert_array);            
+        }
+ 
+
+
+
+    } //end WarFareSquare class
+
+$rest = new RestServer();
+$rest->addServiceClass(WarFareSquare);
+$rest->handle();
