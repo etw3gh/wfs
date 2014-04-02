eaaa5e6e9846fd7d0a1bb0b5559b9436a6cbc879
softcodedotca <etabw3@gmail.com>
2014-04-02 19:31:59 -0400

    favicons added

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo "Nothing to see here, move along now.";
+echo <<<EOD
+<html>
+<head>
+    <title>
+        wfs
+    </title>
+    <link rel='icon' type='image/ico' href='wfs5.ico' />
+</head>
+<body>
+<a href="https://twitter.com/warfaresquare">
+<img src="https://raw.githubusercontent.com/softcodedotca/wfs/master/wfs25.jpg" alt="WarFareSquare"></a>
+</body>
+</html>
+EOD;
